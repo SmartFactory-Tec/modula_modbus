0c873d6acc5001af880db55b0ee13d63e5692c06
A00227526 <113141900+A00227526@users.noreply.github.com>
2022-11-05 12:15:18 -0600

    registros con estados

--- Registros_Modula.py
+++ Registros_Modula.py
@@ -30,18 +30,23 @@ class Registros:
         # Registros = 1-Pedido, 2-Estatus de la charola, 3-Confirmacion de pick para regreso de la charola
         self.informacion=[0,0,0]
 
-    def pedido(self, item):
+    def pedido(self, item): 
         res = requests.post(hostname + output_req_uri, json = item, auth =(user, password))
         #body = res.json()['result']
         #Registros.informacion[0] = body
-        if item == 'hamburguesa':
+        if item == 'fixture':
             self.informacion[0] = 10
             self.send_info()
 
-    def status(self):
+    def status(self): # 1 indica que la bandeja esta en proceso, 2 indica que la bandeja ha llegado, 3 indica un error
         res = requests.get(hostname + tray_stat_uri, json={})
         body = res.json()['result']
-        self.informacion[1] = body
+        if body == '403':
+            self.informacion[1] = 1
+        elif body == '200':
+            self.informacion[1] = 2
+        else:
+            self.informacion[1] = 3
 
     def confirmacion_pedido(self):
         res = requests.post(hostname + req_confirm_uri, json = confirm)
@@ -59,14 +64,14 @@ class Registros:
             print( self.informacion[0])
 
 
-            c.write_multiple_registers(0,[self.informacion[0],self.informacion[1]])
+            c.write_multiple_registers(0,[self.informacion[0],self.informacion[1],self.informacion[2]])
             time.sleep(1)
 
         else:
             print("unable to connect to "+myhost+":"+str(myport))
     
 pedido1 = Registros()
-pedido1.pedido('hamburguesa')
+pedido1.pedido('fixture')
 
 
 
