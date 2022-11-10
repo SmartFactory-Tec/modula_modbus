e4d7fec6caf720c56878419eaaa6b59e39a00d8b
Hiram Muñoz <hirammunozr@outlook.com>
2022-11-10 12:00:52 -0600

    Refactored server setup

--- Modula_modbus_server.py
+++ Modula_modbus_server.py
@@ -4,30 +4,21 @@ from config import load_config
 from odoo_client import OdooClient
 import json
 
-def main():
-    config = load_config()
+# Registro 0 es entrada de producto, registro 1 salida de producto, registro 2 estado de charola, registro 3 devolucion de charola
+# Registro 4 es la posicion en x del producto, registro 5 es la posicion en y del producto, registro 6 es la dimension en x del producto
+# Registro 7 es la dimension en y del producto
+# si el registro 0, o el registro 1 se le da un valor de 1 entonces se realizara un pedido, cuando se le asigne el valor de 2 significara
+# que el modula ya esta procesando el pedido, lo mismo pasa con el registro 3 pero este es para la devolucion de la bandeja
+# para el registro 3 el valor de 1 significara que la bandeja esta en camino y el valor 2 significara que la bandeja ya ha llegado
 
-    # Create an instance of ModbusServer
-    # Registro 0 es entrada de producto, registro 1 salida de producto, registro 2 estado de charola, registro 3 devolucion de charola
-    # Registro 4 es la posicion en x del producto, registro 5 es la posicion en y del producto, registro 6 es la dimension en x del producto
-    # Registro 7 es la dimension en y del producto
-
-    # si el registro 0, o el registro 1 se le da un valor de 1 entonces se realizara un pedido, cuando se le asigne el valor de 2 significara
-    # que el modula ya esta procesando el pedido, lo mismo pasa con el registro 3 pero este es para la devolucion de la bandeja
-
-    # para el registro 3 el valor de 1 significara que la bandeja esta en camino y el valor 2 significara que la bandeja ya ha llegado
-
-    status_modula = {"status": ""}
-
-    client = OdooClient(hostname=config['hostname'], user=config['user'], password=config['password'], secure=config['secure'],
-                               port=config['port'])
-
-    # try:
+def start(server: ModbusServer, client: OdooClient):
     print("Start server...")
-    server = ModbusServer('localhost', 12345, no_block=True)
     server.start()
     print("Server is online")
+
     state = [0]
+    status_modula = {"status": ""}
+
     while True:
         # Creacion de registros y print de los registros
         if state != server.data_bank.get_holding_registers(0, 15):
@@ -80,6 +71,20 @@ def main():
 
         sleep(0.5)
 
+def main():
+    config = load_config()
+
+    client = OdooClient(hostname=config['hostname'], user=config['user'], password=config['password'], secure=config['secure'],
+                               port=config['port'])
+    server = ModbusServer('localhost', 12345, no_block=True)
+
+    try:
+        start(server, client)
+    except KeyboardInterrupt:
+        server.stop()
+    except Exception:
+        server.stop()
+        raise
 
 if __name__ == '__main__':
     main()
