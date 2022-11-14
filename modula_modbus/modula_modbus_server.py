ed71cecb5ba4e12d3f9aa49ad6669d84557940af
A00227526 <113141900+A00227526@users.noreply.github.com>
2022-11-14 17:46:55 -0600

    pruebas

--- modula_modbus/modula_modbus_server.py
+++ modula_modbus/modula_modbus_server.py
@@ -38,41 +38,45 @@ class ModulaDataBank(DataBank):
 
              # Si se llega a tener un pedido de entrada de producto
             if h_registers[0]==1:
-                codigo_bandeja = self.odoo_client.create_input_picking(product_code='fix', quantity= h_registers[2])
+                #codigo_bandeja = self.odoo_client.create_input_picking(product_code='fix', quantity= h_registers[2])
+                codigo_bandeja = 2
                 self.set_holding_registers (1, [codigo_bandeja])
-                self.set_coils(0,0)
-                self.set_discrete_inputs(0,0)
+                self.set_coils(0,[0])
+                self.set_discrete_inputs(0,[0])
             
              # Si se llega a tener un pedido de salida de producto
             if h_registers[0]==2:
-                codigo_bandeja = self.odoo_client.create_output_picking(product_code='fix', quantity= 1)
+                #codigo_bandeja = self.odoo_client.create_output_picking(product_code='fix', quantity= 1)
+                codigo_bandeja = 3
                 self.set_holding_registers (1, [codigo_bandeja])
-                self.set_coils(0,0)
-                self.set_discrete_inputs(0,0)
+                self.set_coils(0,[0])
+                self.set_discrete_inputs(0,[0])
             
             # Revisar el estado de la bandeja y pedir datos adicionales si ya se esta en picking
             if h_registers[0] == 3:
-                status_modula = self.odoo_client.get_tray_status(h_registers[1])
+                #status_modula = self.odoo_client.get_tray_status(h_registers[1])
+                status_modula={'status':"not in picking"}
                 
                 if status_modula['status'] == "not in picking":
-                    self.set_input_registers(0,1)
+                    self.set_input_registers(0,[1])
                 
                 if status_modula ['status'] == "in picking":
                     self.set_input_registers(0, [2, int(status_modula['pos_x']), int(status_modula['pos_y']), int(status_modula['dim_x']),int(status_modula['dim_y'])])
-                self.set_coils(0,0)
-                self.set_discrete_inputs(0,0)
+                self.set_coils(0,[0])
+                self.set_discrete_inputs(0,[0])
 
             # Si se devuelve la bandeja y mandar todos los registros a un estado inicial
             if h_registers[0] == 4:
-                self.odoo_client.confirm_picking(h_registers[1])
+                #self.odoo_client.confirm_picking(h_registers[1])
                 
                 self.set_holding_registers(0,[0,0,0])
-                self.set_coils(0,0)
+                self.set_coils(0,[0])
                 self.set_input_registers(0,[0,0,0,0,0])
-                self.set_discrete_inputs(0,0)
+                self.set_discrete_inputs(0,[0])
 
             pass
 
     def on_holding_registers_change(self, address, from_value, to_value, srv_info):
-        self.set_discrete_inputs(0,1)
+        self.set_discrete_inputs(0,[1])
+
         pass
